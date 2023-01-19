e865c398ca6173db8183d5fba9d7d573a37864eb
Held <Maximilian.Held@adastragrp.com>
2023-01-19 17:17:40 +0100

    iteration2

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 def convert_roman_to_int(roman_string):
-    roman_mapping = {
+    roman_units_mapping = {
         '': 0,
         'I': 1,
         'V': 5,
@@ -7,10 +7,22 @@ def convert_roman_to_int(roman_string):
         'L': 50,
         'C': 100,
         'D': 500,
-        'M': 1000
+        'M': 1000,
+        'IV': 4,
+        'IX': 9,
+        'XL': 40,
+        'XC': 90,
+        'CD': 400,
+        'CM': 900
+
+
     }
-    return roman_mapping[roman_string]
+    return roman_units_mapping[roman_string]
+
+
+#    def unitparser(number):
 
 
 if __name__ == '__main__':
     convert_roman_to_int('PyCharm')
+
--- tests.py
+++ tests.py
@@ -4,31 +4,37 @@ from app import main
 
 
 class TestMain(unittest.TestCase):
-    def test_roman_single_digit(self, expected_result, test_result):
-        self.assertEqual(expected_result, test_result)
+    def test_roman_single_digit(self):
+        self.assertEqual(1, main.convert_roman_to_int('I'))
+        self.assertEqual(5, main.convert_roman_to_int('V'))
+        self.assertEqual(10, main.convert_roman_to_int('X'))
+        self.assertEqual(50, main.convert_roman_to_int('L'))
+        self.assertEqual(100, main.convert_roman_to_int('C'))
+        self.assertEqual(500, main.convert_roman_to_int('D'))
+        self.assertEqual(1000, main.convert_roman_to_int('M'))
 
-    def test_roman_multiple_digit(self, expected_result, test_result):
-        self.assertEqual(expected_result, test_result)
+    def test_substraction_units(self):
+        self.assertEqual(4, main.convert_roman_to_int('IV'))
+        self.assertEqual(9, main.convert_roman_to_int('IX'))
+        self.assertEqual(40, main.convert_roman_to_int('XL'))
+        self.assertEqual(90, main.convert_roman_to_int('XC'))
+        self.assertEqual(400, main.convert_roman_to_int('CD'))
+        self.assertEqual(900, main.convert_roman_to_int('CM'))
+
+    def test_parsing_units(self):
+        self.assertEqual(['I','I'], main.unitparser('II'))
+        self.assertEqual(['XL','IX'], main.unitparser('XLIX'))
+        self.assertEqual(['X','X','I'], main.unitparser('XXI'))
+        self.assertNotEqual(['I','V'], main.unitparser('IV'))
 
     def test_convert_roman_to_int(self):
         # 0
         data = ''
         result = main.convert_roman_to_int(data)
-        self.assertEqual(0, 'result')
+        self.assertEqual(0, result)
 
-
-
-        TestMain.test_roman_single_digit(1, 'I')
-        TestMain.test_roman_single_digit(5, 'V')
-        TestMain.test_roman_single_digit(10, 'X')
-        TestMain.test_roman_single_digit(50, 'L')
-        TestMain.test_roman_single_digit(100, 'C')
-        TestMain.test_roman_single_digit(500, 'D')
-        TestMain.test_roman_single_digit(1000, 'M')
-
-        data = 'VI'
-        result = main.convert_roman_to_int(data)
-        self.assertEqual(6, result)
+        self.test_roman_single_digit()
+        self.test_substraction_units()
 
 
 if __name__ == '__main__':
